7ddc3fae2c460a8666fd715452ef1e3a19d80627
Jeehyung Lee <jeehyungl@gmail.com>
2013-05-31 22:00:22 -0700

    Initial Commit

---
+++ Cleverclovers/IntroLayer.h
@@ -0,0 +1,21 @@
+//
+//  IntroLayer.h
+//  Cleverclovers
+//
+//  Created by Jeehyung Lee on 5/31/13.
+//  Copyright Jeehyung Lee 2013. All rights reserved.
+//
+
+
+// When you import this file, you import all the cocos2d classes
+#import "cocos2d.h"
+
+// HelloWorldLayer
+@interface IntroLayer : CCLayer
+{
+}
+
+// returns a CCScene that contains the HelloWorldLayer as the only child
++(CCScene *) scene;
+
+@end
---
+++ Cleverclovers/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  Cleverclovers
+//
+//  Created by Jeehyung Lee on 5/31/13.
+//  Copyright Jeehyung Lee 2013. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+int main(int argc, char *argv[]) {
+    
+    NSAutoreleasePool * pool = [[NSAutoreleasePool alloc] init];
+    int retVal = UIApplicationMain(argc, argv, nil, @"AppController");
+    [pool release];
+    return retVal;
+}
